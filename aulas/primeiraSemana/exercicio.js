630a666a931a83c5f3882762d7b1f0fbcfe16423
kim-a9 <kimberlysantosalves@gmail.com>
2025-06-10 21:49:11 -0300

    primeiras aulas

---
+++ aulas/primeiraSemana/exercicio.js
@@ -0,0 +1,24 @@
+// const nome = "Maria";
+
+// const idade = 28;
+
+// const admin = false;
+
+// const alunas = ["Juliana", "Ana", "Lais", "Angela"];
+
+// const modulos = {"primeiraSemana": "Fundamentos Back-End", "segundaSemana": "Clean Architecture"}
+
+// let valorNulo = null;
+// let valorIndefinido;
+
+// const temperatura = 25.5;
+// if (temperatura > 20) {
+//     console.log("Está quente");
+// } else {
+//     console.log("Está frio");
+// }
+
+
+for (let i = 0; i < 5; i++) {
+    console.log('Minha repetição de número ${i}'  );
+}
